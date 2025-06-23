af7520b4a75f1160e3b632b4c614d8e2822bfca9
TalipKoyluoglu <talip.koyluoglu.1@ens.etsmtl.ca>
2025-06-23 18:06:04 +0000

    Fix pipeline

--- config/settings.py
+++ config/settings.py
@@ -148,9 +148,9 @@ LOGGING = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
+        "LOCATION": os.getenv("REDIS_URL", "redis://redis:6379/1"),
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
+        }
     }
 }
